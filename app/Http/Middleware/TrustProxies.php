c162907ca9916b9e3d5d4476da1012d863d5203f
davey <davey.rpt@gmail.com>
2025-03-20 01:31:56 +0800

    fix

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -25,11 +25,5 @@ class TrustProxies extends Middleware
         Request::HEADER_X_FORWARDED_PORT |
         Request::HEADER_X_FORWARDED_PROTO |
         Request::HEADER_X_FORWARDED_AWS_ELB;
-
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->trustProxies(at: [
-            '192.168.1.1',
-            '10.0.0.0/8',
-        ]);
-    });
+        
 }
